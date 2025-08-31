584a4f0ad39f0565e602d8de160e6269c9411e52
Musa-Kal <131704022+Musa-Kal@users.noreply.github.com>
2025-08-31 18:32:39 -0400

    sudo implemented order_domain_values

--- crossword/generate.py
+++ crossword/generate.py
@@ -221,7 +221,7 @@ class CrosswordCreator():
         The first value in the list, for example, should be the one
         that rules out the fewest values among the neighbors of `var`.
         """
-        raise NotImplementedError
+        return self.domains[var] # for test purpose will implement later
 
     def select_unassigned_variable(self, assignment):
         """
